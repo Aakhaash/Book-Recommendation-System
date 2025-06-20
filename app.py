4c020f2422c3241b9c9523c85ee9fb63fbe27eeb
Aakhaash <aakhaash77@gmail.com>
2025-06-20 19:14:02 +0530

    Prepare for Render Deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
 from model import popular_df, recommend, books
+import os
 
 app = Flask(__name__)
 
@@ -26,4 +27,5 @@ def recommendation():
     return render_template('recommend.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
